71116cac7b1e115d828faa755e0e0c817e39b0b1
ywais <yuvalwais@gmail.com>
2020-08-05 17:11:47 +0300

    first entry point

--- rest.js
+++ rest.js
@@ -4,8 +4,21 @@ let count = 2;
 
 app.use(express.json());
 
+let products = [
+    {
+        id: 0,
+        name: 'Milk',
+        quantity: 2
+    },
+    {
+        id: 1,
+        name: 'Eggs',
+        quantity: 12
+    }
+];
+
 app.get('/products', (req, res) => {
-    res.send('products');
+    res.send(products);
 });
 
 app.listen(3001);
