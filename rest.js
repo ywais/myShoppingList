38e2f0598e55ce6d02ebd3cdbe8f26a8aca74974
ywais <yuvalwais@gmail.com>
2020-08-05 19:25:10 +0300

    fourth entry point

--- rest.js
+++ rest.js
@@ -23,8 +23,7 @@ app.get('/products', (req, res) => {
 
 app.get('/product/:id', (req, res) => {
     for(let product of products) {
-        if(product.id === req.params.id)
-        {
+        if(product.id === req.params.id) {
             flag = false;
             res.send(product);
         }
@@ -37,8 +36,7 @@ app.get('/product/:id', (req, res) => {
 
 app.post('/product', (req, res) => {
     for(let product of products) {
-        if(product.id === req.body.id)
-        {
+        if(product.id === req.body.id) {
             flag = false;
             res.send('The inserted ID is in use');
         }
@@ -46,6 +44,24 @@ app.post('/product', (req, res) => {
     if(flag) {
         products.push(req.body);
         res.send(req.body)
+    }
+    flag = true;
+});
+
+app.put('/product/:id', (req, res) => {
+    products.forEach((product, index) => {
+        if(product.id === req.params.id) {
+            flag = false;
+            if(req.params.id === req.body.id) {
+                products[index] = req.body;
+                res.send(products[index]);
+            } else {
+                res.send('Do not change the product ID!');
+            }
+        }
+    });
+    if(flag) {
+        res.send('The is no product with this ID');
     }
     flag = true;
 });
